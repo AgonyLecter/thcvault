6885026564bf0866f42c19831302acba2007c1ba
AgonyLecter <142232640+AgonyLecter@users.noreply.github.com>
2024-06-13 22:50:29 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,23 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/riserve.html',
-          '/manifest.json',
-          '/logo.ico',
-          '/cannabis.png', // Aggiungi il file icona
-          '/css/styles.css', // Assuming you have a separate CSS file
-          '/js/app.js' // Assuming you have a separate JS file
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/riserve.html',
+        '/manifest.json',
+        '/logo.ico',
+        '/cannabis.png' // Aggiungi il file icona
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
   
